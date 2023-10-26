ac6b9a0c084d05b7d0a473fca472b94aba4e6c00
SashaKuh <71312391+SashaKuh@users.noreply.github.com>
2023-10-26 14:15:55 +0300

    ts

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,54 @@
-const key = new Key();
+class Key {
+    constructor(private signature: number = Math.random()) {}
 
+    getSignature() {
+        return this.signature;
+    }
+}
+
+
+class Person {
+        constructor(private key: Key) {}
+
+    getKey() {
+        return this.key;
+    }
+}
+
+abstract class House {    
+    constructor(protected door: boolean = false, protected key: Key, protected tenants: Person[] = []) {}
+
+    abstract openDoor(key: Key): void;
+
+    comeIn(person: Person) {
+        if (this.door) {
+            this.tenants.push(person);
+            console.log(`${person.getKey()} inside the house.`);
+        } 
+        console.log('The door is closed. You cannot enter.');
+        }
+    }
+
+
+class MyHouse extends House {
+    constructor(key: Key) {
+        super(false, key)
+    }
+
+    openDoor(key: Key) {
+        if (key.getSignature() === this.key.getSignature()) {
+            this.door = true;
+            console.log('The door is open.');
+        } else {
+            console.log('Invalid key. The door remains closed.');
+        }
+    }
+}
+
+
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
-
 house.comeIn(person);
-
-
-export {};
